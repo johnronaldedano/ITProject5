9c8dfefb05536bc12b1dde2ad3e0c6d53a912396
John Ronald Edaño <2085595@g.cu.edu.ph>
2025-08-19 07:37:23 +0800

    admin_manages changes

--- admin/admin_manage_followups.php
+++ admin/admin_manage_followups.php
@@ -3,8 +3,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="../css/admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
+    <div class="d-flex">
+    <?php include '../includes/sidebar.php'; ?>
+
+                <div class="flex-grow-1 p-4">
+        <?php include '../includes/toggle.php'; ?>
+        <div class="mb-5 mt-5">
+            <h2 class="fw-bold">Dental Patient Record Management System</h2>
+
+        </div>
+</div>
     
+
+</div>
+
+<script src="../js/dashboard.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
