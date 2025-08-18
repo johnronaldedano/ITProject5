0c2a0cd8b97952953570e386e6cb151fbcb5d2fd
John Ronald Edaño <2085595@g.cu.edu.ph>
2025-08-19 03:11:05 +0800

    file name modified on sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -8,11 +8,11 @@ $current_page = basename($_SERVER['PHP_SELF']);
     </div>
     <ul class="nav flex-column">
         <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_dashboard.php') echo ' active'; ?>" href="admin_dashboard.php"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
-        <li class="nav-item"><a class="nav-link<?php if($current_page == 'Subject.php') echo ' active'; ?>" href="Subject.php"><i class="bi bi-person-lines-fill"></i> Manage Patients</a></li>
-        <li class="nav-item"><a class="nav-link<?php if($current_page == 'Classes.php') echo ' active'; ?>" href="Classes.php"><i class="bi bi-person-badge"></i> Manage Doctors</a></li>
-        <li class="nav-item"><a class="nav-link<?php if($current_page == 'Students.php') echo ' active'; ?>" href="Students.php"><i class="bi bi-cash-coin"></i> Manage Revenue</a></li>
-        <li class="nav-item"><a class="nav-link<?php if($current_page == 'Teachers.php') echo ' active'; ?>" href="Teachers.php"><i class="bi bi-arrow-repeat"></i> Manage Follow-ups</a></li>
-        <li class="nav-item"><a class="nav-link<?php if($current_page == 'Attendance.php') echo ' active'; ?>" href="Attendance.php"><i class="bi bi-calendar-check"></i>  Appointments </a></li>
-        <li class="nav-item"><a class="nav-link<?php if($current_page == 'systemuser.php') echo ' active'; ?>" href="systemuser.php"><i class="bi bi-journal-medical"></i> Patient Records </a></li>
+        <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_manage_patient.php') echo ' active'; ?>" href="admin_manage_patient.php"><i class="bi bi-person-lines-fill"></i> Manage Patients</a></li>
+        <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_manage_doctor.php') echo ' active'; ?>" href="admin_manage_doctor.php"><i class="bi bi-person-badge"></i> Manage Doctors</a></li>
+        <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_manage_revenue.php') echo ' active'; ?>" href="admin_manage_revenue.php"><i class="bi bi-cash-coin"></i> Manage Revenue</a></li>
+        <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_manage_followups.php') echo ' active'; ?>" href="admin_manage_followups.php"><i class="bi bi-arrow-repeat"></i> Manage Follow-ups</a></li>
+        <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_manage_appointments.php') echo ' active'; ?>" href="admin_manage_appointments.php"><i class="bi bi-calendar-check"></i>  Appointments </a></li>
+        <li class="nav-item"><a class="nav-link<?php if($current_page == 'admin_manage_records.php') echo ' active'; ?>" href="admin_manage_records.php"><i class="bi bi-journal-medical"></i> Patient Records </a></li>
     </ul>
 </nav>
